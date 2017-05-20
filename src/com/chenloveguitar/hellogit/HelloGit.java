252594e308d060c410461a19f2d739d84de6c92b
陈海东 <chenloveguitar@qq.com>
2017-05-20 10:01:18 +0800

    修改

--- src/com/chenloveguitar/hellogit/HelloGit.java
+++ src/com/chenloveguitar/hellogit/HelloGit.java
@@ -2,6 +2,6 @@ package com.chenloveguitar.hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("Hello Git 123");
+		System.out.println("Hello");
 	}
 }
