f6d4616ea1d03a1b7b06ba71ab1c654c29cee6d9
陈海东 <chenloveguitar@qq.com>
2017-05-20 09:49:59 +0800

    修改

--- src/com/chenloveguitar/hellogit/HelloGit.java
+++ src/com/chenloveguitar/hellogit/HelloGit.java
@@ -2,10 +2,6 @@ package com.chenloveguitar.hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello Git");
-=======
 		System.out.println("Hello SVN");
->>>>>>> branch 'master' of https://github.com/alienCHD/git_hello
 	}
 }
