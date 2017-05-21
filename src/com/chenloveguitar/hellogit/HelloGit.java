e765ae804a2d98627cf0c8e20e00381db7f1b720
home-pc <chenloveguitar@gmail.com>
2017-05-21 23:08:07 +0800

    测试文件的类型

--- src/com/chenloveguitar/hellogit/HelloGit.java
+++ src/com/chenloveguitar/hellogit/HelloGit.java
@@ -1,8 +1,19 @@
 package com.chenloveguitar.hellogit;
 
+import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
+
+import eu.medsea.mimeutil.MimeUtil;
+
 public class HelloGit {
-	public static void main(String[] args) {
-		System.out.println("");
-		
+	public static void main(String[] args) throws Exception {
+		File file = new File("F:/我的录像/2016-06-09 录制/视频2016-06-09 22.01.55.WMV");
+		MimeUtil.registerMimeDetector("eu.medsea.mimeutil.detector.MagicMimeMimeDetector");    
+        Collection<?> mimeTypes = MimeUtil.getMimeTypes(file);
+        Iterator<?> iterator = mimeTypes.iterator();
+        while(iterator.hasNext()){
+        	System.out.println(iterator.next());
+        }
 	}
 }
