00a212587816002e96b03f21782acaf666dd0d79
home-pc <chenloveguitar@gmail.com>
2017-05-21 16:24:18 +0800

    按照我的啊

--- src/com/chenloveguitar/hellogit/HelloGit.java
+++ src/com/chenloveguitar/hellogit/HelloGit.java
@@ -2,10 +2,6 @@ package com.chenloveguitar.hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("输出内容");
-=======
 		System.out.println("hello git");
->>>>>>> branch 'master' of https://github.com/alienCHD/git_hello
 	}
 }
