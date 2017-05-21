d261f507f68341d6c3b6331f1a04eeb08fa6cc02
home-pc <chenloveguitar@gmail.com>
2017-05-21 16:28:06 +0800

    你妈蛋的呢

--- src/com/chenloveguitar/hellogit/HelloGit.java
+++ src/com/chenloveguitar/hellogit/HelloGit.java
@@ -2,6 +2,6 @@ package com.chenloveguitar.hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("hello 你妈蛋");
+		System.out.println("hello 我靠尼玛但");
 	}
 }
