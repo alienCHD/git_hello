8646ac5418c92ae54fa3d7d92292fbb1807fe96a
home-pc <chenloveguitar@gmail.com>
2017-05-21 17:14:54 +0800

    发凡

--- src/com/chenloveguitar/hellogit/HelloGit.java
+++ src/com/chenloveguitar/hellogit/HelloGit.java
@@ -4,5 +4,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("再改我要杀了你");
 		System.err.println();
+		
+		System.out.println("");
+		
+		System.out.println("");
 	}
 }
