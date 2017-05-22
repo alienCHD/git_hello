4d748cf2eee7c81a142f70583486804406e80702
home-pc <chenloveguitar@gmail.com>
2017-05-22 09:10:58 +0800

    哈哈哈

--- src/com/chenloveguitar/hellogit/HelloGit.java
+++ src/com/chenloveguitar/hellogit/HelloGit.java
@@ -3,9 +3,6 @@ package com.chenloveguitar.hellogit;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("");
-		
-		
-		
-		
+		System.out.println("say goodbye!");
 	}
 }
